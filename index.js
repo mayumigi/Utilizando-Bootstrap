ef0942f1bec63e8733f61dd4f161e5cffdd8057c
mayumigi <giovannasaito10@gmail.com>
2025-11-10 16:41:28 -0300

    Base do projeto

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.get('/', (req, res) => {
+    res.render('view/home');
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
