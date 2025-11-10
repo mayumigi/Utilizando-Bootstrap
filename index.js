a8595e7b909f36b1850498be6531fc6ad0c27090
mayumigi <giovannasaito10@gmail.com>
2025-11-10 16:52:58 -0300

    Mudanças

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '/views'));
+
+//Routes
 app.get('/', (req, res) => {
-    res.render('view/home');
+    res.render('home');
 });
 
 app.listen(3000, () => {
